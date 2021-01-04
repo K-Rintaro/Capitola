8e4a4622c7c204ef4262e2193503ba1495413e5c
K-Rintaro <git.rintaro@gmail.com>
2021-01-04 21:03:09 +0900

    feat: super chat text-to-speech

--- index.js
+++ index.js
@@ -1,38 +1,73 @@
 require('dotenv').config();
 const Discord = require("discord.js")
 const client = new Discord.Client();
-const cron = require("node-cron");
 const YouTube = require('youtube-live-chat');
+const googleTTS = require('google-tts-api');
 
-var tokenarr = ['YOUTUBE_TOKEN1','YOUTUBE_TOKEN2']
-ArrayRoops(tokenarr, 120000, function(key, value){
-  const yt = new YouTube('YOUTUBE_CHANNEL_ID', value);
+
+client.on("message", async message => {
+  if (message.content === 'bot.join') {
+    // Only try to join the sender's voice channel if they are in one themselves
+    if (message.member.voice.channel) {
+      const connection = await message.member.voice.channel.join();
+    } else {
+      message.reply('Please join the voice channel first.');
+    }
+  }
+
+  if (message.content === 'bot.leave') {
+    message.member.voice.channel.leave();
+    message.channel.send(`Left from ${message.member.voice.channel.name}`)  
+  }
+});
+
+const yt = new YouTube("YOUTUBE_STREAMER_ID" , process.env.YOUTUBE_TOKEN);
   yt.on('ready', () => {
-  yt.listen(3000)
+  yt.listen(1000)
   })
 
   yt.on('message', data => {
-  console.log("AUT:   " + data.authorDetails.displayName + "MSG:  " + data.snippet.displayMessage)
+  let superchatmsg;
+  if (data.snippet.displayMessage.includes("from")){
+    if(data.snippet.displayMessage.includes(":")){
+      if(data.snippet.displayMessage.includes(data.authorDetails.displayName)){
+        superchatmsg = data.snippet.displayMessage.split(': "')[1]
+        console.log(superchatmsg)
+        client.channels.cache.get('DISCORD_CHANNEL_ID').send("スーパーチャットを受信しました")
+        const url = googleTTS.getAudioUrl(data.authorDetails.displayName + "さんからのスーパーチャット "+ superchatmsg, {
+          lang: 'ja-JP',
+          slow: false,
+          host: 'https://translate.google.com',
+        });
+        console.log(url);
+        if (superchatmsg.length > 200){
+          return
+        }
+        client.on('message', async message => {
+          if (message.content.includes("スーパーチャットを受信しました")) {
+            if (message.member.voice.channel) {
+              const connection = await message.member.voice.channel.join();
+              connection.play(url)
+            }
+          }
+        });
+      }
+    }
+  }
+  client.channels.cache.get('DISCORD_CHANNEL_ID').send(
+    {embed: {
+      author: {
+        name: data.authorDetails.displayName, 
+        icon_url: data.authorDetails.profileImageUrl,
+      },
+      description: data.snippet.displayMessage,
+      timestamp: data.snippet.publishedAt
+    }}
+  )
   })
-
+  
   yt.on('error', error => {
   console.error(error)
   })
-});
-
-function ArrayRoops(array, time, fun){
-    this.length = array.length;
-    this.counts = 0;
-    this.Timer = setInterval(() => {
-        this.key = this.counts;
-        this.value = array[this.key];
-        fun(this.key, this.value);
-        this.counts ++;
-        if(this.length <= this.counts){
-            this.counts = 0;
-            clearInterval(this.Timer);
-        }
-    }, time);
-}
 
 client.login(process.env.DISCORD_TOKEN);
